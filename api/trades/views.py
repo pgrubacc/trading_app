f5b5fb0bdc19e6d639058cfea33712a8935fadf7
Petar Grubac <pgrubacc@gmail.com>
2020-03-01 12:18:53 +0100

    Add Swagger API docstrings

--- api/trades/views.py
+++ api/trades/views.py
@@ -9,17 +9,37 @@ from trades.utils import get_currency_query_param, get_exchange_rate
 
 
 class TradeList(ListCreateAPIView):
+    """
+    View used to list and create trades.
+
+    get:
+    Gets the list of trades sorted by date booked, descending.
+
+    post:
+    Creates a new trade.
+    """
     queryset = Trade.objects.select_related('sell_currency', 'buy_currency').\
         order_by('-date_booked')
     serializer_class = TradeSerializer
 
 
 class CurrencyList(ListAPIView):
+    """
+    Get the list of supported currencies, sorted by name.
+    """
     queryset = Currency.objects.order_by('name')
     serializer_class = CurrencySerializer
 
 
 class ExchangeRate(APIView):
+    """
+    Get the exchange rate between two currencies.
+     Parameters:
+        sell - name of the currency being sold (MANDATORY, name must exist in the currency table)
+        buy - name of the currency being bought (MANDATORY, name must exist in the currency table)
+
+    http://localhost:8002/api/v1.0/rates/?sell=EUR&buy=USD
+    """
     def get(self, request):
         sell_currency = get_currency_query_param(request, 'sell')
         buy_currency = get_currency_query_param(request, 'buy')
